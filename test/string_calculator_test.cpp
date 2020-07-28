3ae4e0d6a98b425e51ff08827e39ccd7d54dc091
Bartolome Jimenez Vera <bartolomehjvera@gmail.com>
2020-07-28 12:45:47 +0000

    Throw exception if single value with comma

--- test/string_calculator_test.cpp
+++ test/string_calculator_test.cpp
@@ -11,10 +11,14 @@ namespace string_calculator
         {
             return 0;
         }
+        if (something.find(",") != std::string::npos)
+        {
+            std::stoi(something.substr(0, something.find(",")));
+            std::stoi(something.substr(something.find(",") + 1));
+        }
         return std::stoi(something);
     }
 } // namespace string_calculator
-
 
 TEST(TestStringCalculator, Should_returnZeroWhenEmptyString)
 {
@@ -22,17 +26,20 @@ TEST(TestStringCalculator, Should_returnZeroWhenEmptyString)
     EXPECT_EQ(0, result);
 }
 
-class StringCalculatorParameterizedTestFixture : public ::testing::TestWithParam<std::string> {};
-TEST_P(StringCalculatorParameterizedTestFixture, Should_returnElementWhenSingleNumberInString)
+class StringCalculatorSingleElementParameterizedTestFixture : public ::testing::TestWithParam<std::string>
+{
+};
+TEST_P(StringCalculatorSingleElementParameterizedTestFixture, Should_returnElementWhenSingleNumberInString)
 {
     std::string expected = GetParam();
     int result = string_calculator::add(expected);
     EXPECT_EQ(std::stoi(expected), result);
 }
 
-std::vector<std::string> get_single_number_strings_in_range(int beg, int end){
+std::vector<std::string> get_single_number_strings_in_range(int beg, int end)
+{
     std::vector<std::string> result;
-    for(int i = beg; i <= end; i++)
+    for (int i = beg; i <= end; i++)
     {
         result.push_back(std::to_string(i));
     }
@@ -41,6 +48,18 @@ std::vector<std::string> get_single_number_strings_in_range(int beg, int end){
 
 INSTANTIATE_TEST_SUITE_P(
     SingleNumberStringTests,
-    StringCalculatorParameterizedTestFixture,
-    ::testing::ValuesIn(get_single_number_strings_in_range(1,20))
-);
+    StringCalculatorSingleElementParameterizedTestFixture,
+    ::testing::ValuesIn(get_single_number_strings_in_range(1, 20)));
+
+class StringCalculatorSingleElementWithCommaParameterizedTestFixture : public ::testing::TestWithParam<std::string>
+{
+};
+TEST_P(StringCalculatorSingleElementWithCommaParameterizedTestFixture, Should_throwExceptionWhenStringWithCommaAndNotTwoElements)
+{
+    ASSERT_THROW(string_calculator::add(GetParam()), std::invalid_argument);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    SingleNumberWithCommaTests,
+    StringCalculatorSingleElementWithCommaParameterizedTestFixture,
+    ::testing::Values(",", "1,", ",2"));
