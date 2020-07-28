91a8035d8d376d94882accb8d9c25ee17cebeadb
Bartolome Jimenez Vera <bartolomehjvera@gmail.com>
2020-07-28 13:26:26 +0200

    Added empty string condition and one number

--- test/string_calculator_test.cpp
+++ test/string_calculator_test.cpp
@@ -1,6 +1,28 @@
+
 #include "gtest/gtest.h"
 
-TEST(TestStringCalculator, Should_Fail)
+#include <string>
+
+namespace string_calculator
 {
-    EXPECT_EQ(1, 0) << "I'm just failing";
+    inline int add(std::string something)
+    {
+        if (something.empty())
+        {
+            return 0;
+        }
+        return std::stoi(something);
+    }
+} // namespace string_calculator
+
+TEST(TestStringCalculator, Should_returnZeroWhenEmptyString)
+{
+    int result = string_calculator::add("");
+    EXPECT_EQ(0, result);
+}
+
+TEST(TestStringCalculator, Should_returnOneWhenSingleNumberIsOne)
+{
+    int result = string_calculator::add("1");
+    EXPECT_EQ(1, result);
 }
